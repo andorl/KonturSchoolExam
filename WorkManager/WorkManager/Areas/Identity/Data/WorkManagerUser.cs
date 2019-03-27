26069de454f3e421d3a6ecda7406f5ebdf48ad3f
Andrey Orlovsky <andrey.orlovsky@gmail.com>
2019-03-28 00:55:03 +0700

    added custom user & dbcontext  classes

---
+++ WorkManager/WorkManager/Areas/Identity/Data/WorkManagerUser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace WorkManager.Areas.Identity.Data
+{
+    // Add profile data for application users by adding properties to the WorkManagerUser class
+    public class WorkManagerUser : IdentityUser
+    {
+    }
+}
---
+++ WorkManager/WorkManager/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using WorkManager.Areas.Identity.Data;
+using WorkManager.Models;
+
+[assembly: HostingStartup(typeof(WorkManager.Areas.Identity.IdentityHostingStartup))]
+namespace WorkManager.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<WorkManagerDbContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("WorkManagerDbContextConnection")));
+
+                services.AddDefaultIdentity<WorkManagerUser>()
+                    .AddEntityFrameworkStores<WorkManagerDbContext>();
+            });
+        }
+    }
+}
